41e0799e1e9697ece6ac163e127b76365a750fbf
Farry <newfarry@126.com>
2019-08-22 12:38:15 +0800

    启动组件之前先检查本网络是否已经存在相同类型的组件

--- component/mqttd.go
+++ component/mqttd.go
@@ -8,7 +8,15 @@ import (
 )
 
 func init() {
+	var componentName = "mqtt"
 	//TODO 判断本网络是否已经存在此类型的组件，存在则不启动
+	exist, err := CheckComponentExist(componentName)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if exist {
+		fmt.Printf("本网络已经存在了%s组件，不启动/n", componentName)
+	}
 	svr := &service.Server{
 		KeepAlive:        3600,
 		ConnectTimeout:   30,
@@ -19,14 +27,15 @@ func init() {
 	}
 	port := 1884
 	mqttaddr := fmt.Sprintf("tcp://0.0.0.0:%d", port)
-	go func() {
-		err := svr.ListenAndServe(mqttaddr)
-		if err != nil {
-			fmt.Printf("surgemq: %v", err)
-		}
-	}()
-	_, err := zeroconf.Register("mqtt", "_component._tcp", "local.", port, []string{}, nil)
+	server, err := zeroconf.Register(componentName, "_component._tcp", "local.", port, []string{}, nil)
 	if err != nil {
 		fmt.Printf("zeroconf: %v", err)
+		return
+	}
+	err = svr.ListenAndServe(mqttaddr)
+	if err != nil {
+		fmt.Printf("surgemq: %v", err)
+		server.Shutdown()
+		return
 	}
 }
---
+++ component/utils.go
@@ -0,0 +1,35 @@
+package component
+
+import (
+	"context"
+	"github.com/grandcat/zeroconf"
+	"log"
+	"time"
+)
+
+func CheckComponentExist(instance string) (bool, error) {
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		log.Fatalln("Failed to initialize resolver:", err.Error())
+		return false, err
+	}
+
+	entries := make(chan *zeroconf.ServiceEntry, 100)
+	defer close(entries)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(2))
+	defer cancel()
+	err = resolver.Browse(ctx, "_component._tcp", "local", entries)
+	if err != nil {
+		log.Fatalln("Failed to browse:", err.Error())
+		return false, err
+	}
+
+	<-ctx.Done()
+	for entry := range entries {
+		if entry.Instance == instance {
+			return true, nil
+		}
+	}
+	return false, nil
+}
