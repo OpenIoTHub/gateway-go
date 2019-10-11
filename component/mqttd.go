cafef96926927140ccb7df107fb7f36bcd66663e
Farry <newfarry@126.com>
2019-10-12 00:32:46 +0800

    由工具函数注册mdns服务

--- component/mqttd.go
+++ component/mqttd.go
@@ -4,17 +4,20 @@ package component
 import (
 	"fmt"
 	nettool "git.iotserv.com/iotserv/utils/net"
-	"github.com/grandcat/zeroconf"
-	"github.com/satori/go.uuid"
 	"github.com/surgemq/surgemq/service"
 )
 
 func init() {
 	go func() {
+		var txtInfo = nettool.MDNSServiceBaseInfo
 		var model = "com.iotserv.devices.mqttd"
-		var port = 1884
+		port, err := nettool.GetOneFreeTcpPort()
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
 		//TODO 判断本网络是否已经存在此类型的组件，存在则不启动
-		exist, err := CheckComponentExist(model)
+		exist, err := nettool.CheckComponentExist(model)
 		if err != nil {
 			fmt.Println(err.Error())
 		}
@@ -31,37 +34,9 @@ func init() {
 			TopicsProvider:   "mem",
 		}
 		mqttaddr := fmt.Sprintf("tcp://0.0.0.0:%d", port)
-		var mac = uuid.Must(uuid.NewV4()).String()
-		fmt.Println("mac:", mac)
-		macs, err := nettool.GetMacs()
-		fmt.Println("macs:", macs)
-		if err == nil && len(macs) > 0 {
-			fmt.Println("mac-len:", len(macs))
-			for _, vMac := range macs {
-				if vMac != "" {
-					mac = vMac
-				}
-			}
-		}
-		var txt = []string{
-			"name=mqtt服务器",
-			fmt.Sprintf("model=%s", model),
-			fmt.Sprintf("mac=%s", mac),
-			fmt.Sprintf("id=%s", uuid.Must(uuid.NewV4()).String()),
-			//web,native,none
-			"ui-support=none",
-			"ui-first=none",
-			"author=Farry",
-			"email=newfarry@126.com",
-			"home-page=https://github.com/iotdevice",
-			"firmware-respository=https://github.com/OpenIoTHub/GateWay",
-			"firmware-version=1.0",
-		}
-		server, err := zeroconf.Register(fmt.Sprintf("%s-%s", model, mac), "_iotdevice._tcp", "local.", port, txt, nil)
-		if err != nil {
-			fmt.Printf("zeroconf: %v", err)
-			return
-		}
+		txtInfo["name"] = "mqtt服务器"
+		txtInfo["model"] = model
+		server, err := nettool.RegistermDNSService(txtInfo, port)
 		err = svr.ListenAndServe(mqttaddr)
 		if err != nil {
 			fmt.Printf("surgemq: %v", err)
