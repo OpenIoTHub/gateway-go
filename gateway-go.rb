9cf9242ba6d2c9138117612d06cb881e12e15948
iotserv <yu@iotserv.com>
2020-09-06 22:41:41 +0800

    update:homebrew rb file

--- gateway-go.rb
+++ gateway-go.rb
@@ -2,16 +2,52 @@ class GatewayGo < Formula
   desc "GateWay Client for OpenIoTHub"
   homepage "https://github.com/OpenIoTHub"
   url "https://github.com/OpenIoTHub/gateway-go.git",
-      :tag      => "v0.1.82",
-      :revision => "00ef621e82242a170181c057244756931607e955"
+      tag:      "v0.1.91",
+      revision: "8df96b8ae676344c14277c61ee8ac8bb206d8ef9"
   license "MIT"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5263c644db3dec3a9163ffcc71b1f99c75730ba1275a8a9bc2affcb6ef479f59" => :catalina
+    sha256 "74351f8be7f640eba1a1742938539db1d8be37b9f2e63dae58a55e7d387a7e9b" => :mojave
+    sha256 "6038aee8b37e90dc5582c000d8fa1845ccab252190c22cbf6af8a472fb55a14d" => :high_sierra
+  end
 
   depends_on "go" => :build
 
   def install
+    (etc/"gateway-go").mkpath
     system "go", "build", "-mod=vendor", "-ldflags",
              "-s -w -X main.version=#{version} -X main.commit=#{stable.specs[:revision]} -X main.builtBy=homebrew",
              *std_go_args
+    etc.install "gateway-go.yaml" => "gateway-go/gateway-go.yaml"
+  end
+
+  plist_options manual: "gateway-go -c #{HOMEBREW_PREFIX}/etc/gateway-go/gateway-go.yaml"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>KeepAlive</key>
+          <true/>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{opt_bin}/gateway-go</string>
+            <string>-c</string>
+            <string>#{etc}/gateway-go/gateway-go.yaml</string>
+          </array>
+          <key>StandardErrorPath</key>
+          <string>#{var}/log/gateway-go.log</string>
+          <key>StandardOutPath</key>
+          <string>#{var}/log/gateway-go.log</string>
+        </dict>
+      </plist>
+    EOS
   end
 
   test do
--- vendor/github.com/libp2p/go-msgio/go.mod
+++ vendor/github.com/libp2p/go-msgio/go.mod
@@ -1,3 +1,5 @@
 module github.com/libp2p/go-msgio
 
+go 1.15
+
 require github.com/libp2p/go-buffer-pool v0.0.1
