c4301c807bc7a190a9c9034f52d06d381f5a969d
Yu Fang <yu@iotserv.com>
2021-07-31 10:45:39 +0800

    获取提交到openwrthash脚本

--- hash.sh
+++ hash.sh
@@ -1,2 +1,7 @@
-openssl sha256 v0.1.92.tar.gz
+#提交openwrt版获取hash
+PKG_VERSION=0.1.92
+URL=https://codeload.github.com/OpenIoTHub/gateway-go/tar.gz/v${PKG_VERSION}
+wget ${URL}
+openssl sha256 v${PKG_VERSION}
+rm v${PKG_VERSION}
 #SHA256(v0.1.92.tar.gz)= dd8074d9312e00ff957ffd1f3be7ba118a9b8cc31f07aa1ed594ef07931dab16
