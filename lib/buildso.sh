e70d1083d6f1c860610fbae2b00b75cf3bdcb1fa
Farry <yu@iotserv.com>
2023-08-26 15:27:05 +0800

    update

--- lib/buildso.sh
+++ lib/buildso.sh
@@ -2,4 +2,6 @@
 echo "building linux/android so file"
 #linux和Android共用动态链接库
 CGO_ENABLED=1 GOARCH=amd64 GOOS=linux go build -tags linux -ldflags=-w -trimpath -v -o "build/amd64/linux/gateway.so" -buildmode c-shared
-CGO_ENABLED=1 GOARCH=arm64 GOOS=linux go build -tags linux -ldflags=-w -trimpath -v -o "build/arm64/linux/gateway.so" -buildmode c-shared
+#sudo apt-get install binutils-aarch64-linux-gnu
+CGO_ENABLED=1 GOARCH=arm64 GOOS=linux CC=aarch64-linux-gnu-gcc && \
+CXX=aarch64-linux-gnu-g++ AR=aarch64-linux-gnu-ar go build -tags linux -ldflags=-w -trimpath -v -o "build/arm64/linux/gateway.so" -buildmode c-shared
