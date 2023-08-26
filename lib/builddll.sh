499a7f72d7f368e05ba63ca424f38c897ea8e214
Farry <yu@iotserv.com>
2023-08-26 18:13:42 +0800

    update

--- lib/builddll.sh
+++ lib/builddll.sh
@@ -1,5 +1,6 @@
 #for build windows dll
 echo "building windows dll"
 #brew install mingw-w64
+#sudo apt-get install binutils-mingw-w64
 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ GOOS=windows GOARCH=amd64 && \
 go build -o ./build/amd64/windows/gateway.dll -buildmode=c-shared
