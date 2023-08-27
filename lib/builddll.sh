05bb02d96b6c01a3b62aefd20c93ee0c029fba24
iotserv <newfarry@126.com>
2023-08-28 01:28:59 +0800

    update ci script

--- lib/builddll.sh
+++ lib/builddll.sh
@@ -3,8 +3,6 @@ echo "building windows dll"
 #brew install mingw-w64
 #sudo apt-get install binutils-mingw-w64
 # shellcheck disable=SC2034
-ls /usr/bin
-which x86_64-w64-mingw32-gcc
 export CGO_ENABLED=1
 export CC=x86_64-w64-mingw32-gcc
 export CXX=x86_64-w64-mingw32-g++
