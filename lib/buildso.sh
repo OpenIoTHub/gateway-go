ce396d26cfd5ced6af9bae1b63c7fcd80679c3bd
Farry <yu@iotserv.com>
2023-08-27 18:48:00 +0800

    update linux/android build scripts

--- lib/buildso.sh
+++ lib/buildso.sh
@@ -11,10 +11,10 @@ export GOARCH=arm64
 export GOOS=linux
 #sudo apt install gcc-aarch64-linux-gnu
 export CC=aarch64-linux-gnu-gcc
-#sudo apt install g++-aarch64-linux-gnu
-export CXX=aarch64-linux-gnu-g++
-#sudo apt-get install binutils-aarch64-linux-gnu
-export AR=aarch64-linux-gnu-ar
+##sudo apt install g++-aarch64-linux-gnu
+#export CXX=aarch64-linux-gnu-g++
+##sudo apt-get install binutils-aarch64-linux-gnu
+#export AR=aarch64-linux-gnu-ar
 go build -tags linux -ldflags=-w -trimpath -o build/linux/gateway_arm64.so -buildmode c-shared main.go
 
 export PATH=~/Android/Sdk/ndk/25.2.9519653/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH
@@ -24,25 +24,25 @@ export CGO_ENABLED=1
 export GOARCH=arm
 export GOOS=android
 export CC=armv7a-linux-androideabi33-clang
-go build -tags android -ldflags=-w -trimpath -o build/android/gateway_arm.so -buildmode c-shared main.go
+go build -tags android -ldflags=-w -trimpath -o build/android/armeabi-v7a/gateway.so -buildmode c-shared main.go
 #android/app/src/main/jniLibs/arm64-v8a
 # shellcheck disable=SC2034
 export CGO_ENABLED=1
 export GOARCH=arm64
 export GOOS=android
 export CC=aarch64-linux-android33-clang
-go build -tags android -ldflags=-w -trimpath -o build/android/gateway_arm64.so -buildmode c-shared main.go
+go build -tags android -ldflags=-w -trimpath -o build/android/arm64-v8a/gateway.so -buildmode c-shared main.go
 #android/app/src/main/jniLibs/x86
 # shellcheck disable=SC2034
 export CGO_ENABLED=1
 export GOARCH=386
 export GOOS=android
 export CC=i686-linux-android33-clang
-go build -tags android -ldflags=-w -trimpath -o build/android/gateway_i386.so -buildmode c-shared main.go
+go build -tags android -ldflags=-w -trimpath -o build/android/x86/gateway.so -buildmode c-shared main.go
 #android/app/src/main/jniLibs/x86_64
 # shellcheck disable=SC2034
 export CGO_ENABLED=1
 export GOARCH=amd64
 export GOOS=android
 export CC=x86_64-linux-android33-clang
-go build -tags android -ldflags=-w -trimpath -o build/android/gateway_amd64.so -buildmode c-shared main.go
+go build -tags android -ldflags=-w -trimpath -o build/android/x86_64/gateway.so -buildmode c-shared main.go
