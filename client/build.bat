9de3f68422b4bfcfb2b941c561bb440d657f153f
iotserv <newfarry@126.com>
2023-09-09 22:51:18 +0800

    upload aar

--- client/build.bat
+++ client/build.bat
@@ -1,2 +1,4 @@
-gomobile bind -target=android
+gomobile bind -target=android  -javapkg=cloud.iothub
+:: gpg --clearsign gateway-0.0.1.aar
+:: mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=aar -DpomFile=gateway-0.0.2.pom -Dfile=gateway-0.0.2.aar
 gomobile bind -ldflags '-w -s -extldflags "-lresolve"' --target=ios,macos,iossimulator
