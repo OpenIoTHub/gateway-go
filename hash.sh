e92e163340eb0f367a83785f9ab05cb16843c757
Yu Fang <newfarry@126.com>
2025-07-28 15:00:38 +0800

    openwrt

--- hash.sh
+++ hash.sh
@@ -1,7 +1,9 @@
 #提交openwrt版获取hash
-PKG_VERSION=0.1.92
-URL=https://codeload.github.com/OpenIoTHub/gateway-go/v2/tar.gz/v${PKG_VERSION}
+PKG_VERSION=2.0.10
+URL=https://codeload.github.com/OpenIoTHub/gateway-go/tar.gz/v${PKG_VERSION}
 wget ${URL}
 openssl sha256 v${PKG_VERSION}
 rm v${PKG_VERSION}
-#SHA256(v0.1.92.tar.gz)= dd8074d9312e00ff957ffd1f3be7ba118a9b8cc31f07aa1ed594ef07931dab16
+#SHA2-256(v2.0.10)= 95de453e76a22ae69a1bc9c32d6930278980c98decbe7a511fd9870bcdf90d2d
+
+git commit --amend -m "gateway-go: update to 2.0.10" -s
